7f2ba1b2dc6f92e65b56b1144bed74aeaf0a5e81
tanmaydixitpython7 <backup.tanmay10@gmail.com>
2024-09-12 00:08:15 +0530

    added fibo changes

---
+++ fibo_memorization.py
@@ -0,0 +1,19 @@
+class memorization_fibo:
+    print('fibo function')
+    def __init__(self, rn):
+        self.rn = rn
+
+    def fibo(self,n, memo = {}):
+        if n in memo:
+            return memo[n]
+        elif n<=1:
+            return n
+        memo[n] = self.fibo(n-1, memo) + self.fibo(n-2, memo)
+        return memo[n]
+
+    def fibo_series(self):
+        for i in range(self.rn):
+            print(self.fibo(i))
+
+obj = memorization_fibo(10)
+obj.fibo_series() 
